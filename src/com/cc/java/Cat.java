15c79a5bda5e7bdba7a0b5dc1cba0720548f17af
Peter <schmidt.multimedia@gmail.com>
2021-12-06 12:12:51 +0100

    Cat / TonCat / SuperCat

--- src/com/cc/java/Cat.java
+++ src/com/cc/java/Cat.java
@@ -1,28 +1,11 @@
 package com.cc.java;
 
-public class Cat {
+public class Cat extends SuperCat{
     
-    private String name;
-    private String furColor;
-    private int age;
-
     private int counter;
 
     public Cat(String name, String furColor, int age) {
-        this.name = name;
-        this.furColor = furColor;
-        this.age = age;
-    }
-
-    public String getStringAttributes(String flag){
-        switch (flag) {
-            case "#name":
-                return name;
-            case "#color":
-                return furColor;
-            default:
-                return "#!?";
-        }  
+     super(name,furColor,age);
     }
 
     public String getAge(){
--- src/com/cc/java/Tomcat.java
+++ src/com/cc/java/Tomcat.java
@@ -1,26 +1,10 @@
 package com.cc.java;
 
-public class Tomcat {
-    
-    private String name;
-    private String furColor;
-    private int age;
+public class Tomcat extends SuperCat{
+
   
     public Tomcat(String name, String furColor, int age) {
-        this.name = name;
-        this.furColor = furColor;
-        this.age = age;
-    }
-
-    public String getStringAttributes(String flag){
-        switch (flag) {
-            case "#name":
-                return name;
-            case "#color":
-                return furColor;
-            default:
-                return "#!?";
-        }  
+        super(name, furColor, age);
     }
 
     public String getAge(){
